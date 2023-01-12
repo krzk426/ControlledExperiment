72322c155f7e8d655709c140a48fa73795dc4b5c
krzk426 <86722318+krzk426@users.noreply.github.com>
2023-01-12 10:38:02 +0100

    Update CurrencyScannerNBP.py
    review

--- CodeToReview/CurrencyScannerNBP.py
+++ CodeToReview/CurrencyScannerNBP.py
@@ -12,9 +12,15 @@ CODE_TAG = "code"
 MID_TAG = "mid"
 LAST_CAR = "Mazda"
 
+#what that class is actually doing? remove that one
 class MyClass:
     pass
 
+#that class is quite complicated, I would suggest to split it into few classes, where each would be responsible for one action
+#1) CurrencyRates
+#2) combine to json could be part of class connected to json behaviour - like I said in the second file - create superclass and classes connected to output format 
+# which will inherited from superclass - then ceate method 'saveOutputFile()' in each of them - there it should have convertion. And will be mor euniversal, 
+# in main class programmer will have to do only 'saveOutputFile' without carrying about the output_type
 class CurrencyScanner():
 
     @classmethod
@@ -54,9 +60,10 @@ class CurrencyScanner():
         list_of_rates = []
 
         for table in table_list:
+            #for me it would be more clear if the validation of format would be moved to external method 
             if not isinstance(table, dict):
                 raise ValueError("table must be a dictionary")
-
+    
             effective_date = table.get(EFFECTIVE_DATE_TAG)
             if not isinstance(effective_date, str):
                 raise ValueError('effectiveDate must be a string')
@@ -69,13 +76,17 @@ class CurrencyScanner():
 
         o[EFFECTIVE_DATE_TAG] = table_list[0][EFFECTIVE_DATE_TAG]
         o[RATES_TAG] = list_of_rates
-
+        
+        #why do you have buffer var if you are using it in condition below, and then not returning, not use anywhere else? 
+        #additionally you are assigning value, then validating it and then reassigning 
         buffer = o
         if not isinstance(o, dict): buffer = 0
         
         return o
 
     def hello() -> str:
+        #magic number 42 - wtf 
+        #not used method
         return 42
 
     def jsonToXml(self, d, o_type="normal"):
@@ -92,9 +103,11 @@ class CurrencyScanner():
 
         root = xml.Element(ROOT_TAG)
         xml.SubElement(root, EFFECTIVE_DATE_TAG).text = effective_date
+        #ehat does xy32 name means?
         xy32 = xml.SubElement(root, RATES_TAG)
 
         for curr in rates:
+            #again - move validation to external method
             if not isinstance(curr, dict):
                 raise ValueError("rate must be a dictionary")
 
@@ -119,6 +132,7 @@ class CurrencyScanner():
     
     def watermark(self, data, file_name :str ='data', file_format :str='xml'):
         try:
+            #if you would have two different calsses responsible for outoutFormat it would be one method in each without any conditions
             if file_format == 'json':
                 if not isinstance(data, dict):
                     raise ValueError("data must be a dictionary for file format 'json'")
@@ -127,8 +141,10 @@ class CurrencyScanner():
             elif file_format == 'xml':
                 if not isinstance(xml.ElementTree(data), xml.ElementTree):
                     raise ValueError("data must be an ElementTree for file format 'xml'")
-                data.write(f'{file_name}.{file_format}')
-            elif file_format == 'csv':
+                data.write(f'{file_name}.{file_format}') 
+        #like really xd so create a class and enum value for that and print it there - and do not raise an exception below for that one if its clearly
+        #not an exception, just add a neutral behaviour for that one
+        elif file_format == 'csv':
                 pass # to be added in future releases
             else:
                 raise ValueError("Unsupported file format: {}".format(file_format))
@@ -138,7 +154,8 @@ class CurrencyScanner():
         finally: 
             if(file_format == 'csv'):
                 raise
-    
+    #watermark and watermark_json and watermark_xml - is this once saving and once getting this watermark? or what exactly. I don't know
+    #from the functions name what you want to acheive in here
     def watermark_json(self, data, file_name: str='data'):
         """Write the given data to a file in the specified format.
 
@@ -163,6 +180,7 @@ class CurrencyScanner():
                     raise ValueError("data must be an ElementTree for file format 'xml'")
                 data.write(f'{file_name}.{file_format}')
             elif file_format == 'csv':
+                
                 pass # to be added in future releases
             else:
                 raise ValueError("Unsupported file format: {}".format(file_format))
